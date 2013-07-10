18db89d0d0b95539c49acf967bf09bcb76437abc
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2013-07-10 10:53:49 -0700

    Added new constraints: < <= > >= element-of

--- src/clocop/constraints.clj
+++ src/clocop/constraints.clj
@@ -4,9 +4,14 @@
     (JaCoP.core Var IntVar)
     (JaCoP.constraints XeqY
                        XeqC
+                       XltY
+                       XlteqY
+                       XgtY
+                       XgteqY
                        XneqY
                        XneqC
-                       Alldifferent)))
+                       Alldifferent
+                       Element)))
 
 (defn =%
   "Specifies an equality constraint. At least one of the arguments must be a Var. The non-Var argument (if any) is a constant integer that will be assigned to the Var."
@@ -30,3 +35,33 @@
   "Specifies an all-different constraint."
   [vars]
   (Alldifferent. (into-array IntVar vars)))
+
+(defn element-of%
+  "Given an array (any seq) and two vars (i and x), specifies that the ith item in the array is equal to x.
+Note that the array can either contain only vars, or contain only concrete numbers.
+
+Another important thing is that by default, this array lookup is one-based. Add \":zero-based true\" to make it zero-based."
+  [array i x & {zero-based :zero-based}]
+  (let [array (if (instance? Var (first array))
+                (into-array IntVar array)
+                (int-array array))]
+    (if zero-based
+      (Element. i array x 1)
+      (Element. i array x))))
+
+(defn <%
+  "Specifies that X must be less than Y."
+  [X Y]
+  (XltY. X Y))
+(defn <=%
+  "Specifies that X must be less than or equal to Y."
+  [X Y]
+  (XlteqY. X Y))
+(defn >%
+  "Specifies that X must be greater than Y."
+  [X Y]
+  (XgtY. X Y))
+(defn >=%
+  "Specifies that X must be greater than or equal to Y."
+  [X Y]
+  (XgteqY. X Y))
