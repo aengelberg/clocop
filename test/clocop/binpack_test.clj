cf080205686eeaddf9da5a7cd21e9d2dde24ff36
Alex Engelberg <alex.benjamin.engelberg@gmail.com>
2013-09-05 16:20:41 -0700

    added binpacking test

---
+++ test/clocop/binpack_test.clj
@@ -0,0 +1,21 @@
+(ns clocop.binpack-test
+  (:use clocop.core
+        clocop.constraints
+        clojure.test))
+
+(deftest binpacktest1
+  (with-store (store)
+    (let [x (int-var "xloc" 0 2) ; which bin, from 0 to 2, is x in
+          y (int-var "yloc" 0 2)
+          z (int-var "zloc" 0 2)
+          bins [1 2 3] ; the constant bin capacities
+          xweight (int-var 1) ; x's weight
+          yweight (int-var 3)
+          zweight (int-var 2)]
+      (constrain! ($binpacking :bins bins
+                               :sizes [xweight yweight zweight]
+                               :locations [x y z]))
+      (is (= (solve!)
+             {"xloc" 0
+              "yloc" 2
+              "zloc" 1})))))
